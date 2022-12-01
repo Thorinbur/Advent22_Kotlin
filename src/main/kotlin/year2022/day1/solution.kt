fa30d4b3f990a34609e5758ea5df6cce6811bfb6
Thorinbur <thorinbur@gmail.com>
2022-12-01 22:58:40 +0100

    renamed problem1 package to day1 fixed typo added doc for extension method

--- src/main/kotlin/year2022/day1/solution.kt
+++ src/main/kotlin/year2022/day1/solution.kt
@@ -1,18 +1,21 @@
-package year2022.problem1.a
+package year2022.day1
 
 import java.io.File
 
 fun main() {
-    val data = File("src/main/kotlin/year2022/problem1/1.in").readLines()
+    val data = File("src/main/kotlin/year2022/day1/1.in").readLines()
     val grouped = data.split { it.isBlank() }
     val sums = grouped.map { it.sumOf { line -> line.toInt() } }
 
     fun getTopN(n: Int) = sums.sortedDescending().take(n).sum()
 
     println("part 1 solution: ${getTopN(1)}")
-    println("part 1 solution: ${getTopN(3)}")
+    println("part 2 solution: ${getTopN(3)}")
 }
 
+/** Splits a list on items matching predicate
+ *  Items matching predicate are NOT included in the resulting lists
+ */
 private fun <E> List<E>.split(predicate: (E) -> Boolean) =
     fold(mutableListOf<MutableList<E>>(mutableListOf())) { acc, element ->
         if (predicate(element)) acc.apply { this.add(mutableListOf()) }
