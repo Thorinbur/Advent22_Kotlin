c1e4f002cfb6afa96ae8a4ba450396b26b5374b5
Thorinbur <thorinbur@gmail.com>
2022-12-03 22:58:40 +0100

    day3 solutions

---
+++ src/main/kotlin/year2022/day3/solution.kt
@@ -0,0 +1,20 @@
+package year2022.day3
+
+import java.io.File
+
+fun Char.priority() =  (if (isLowerCase()) (this - 'a' + 1)
+else this - 'A' + 27).toInt()
+
+fun main() {
+    val sacks = File("src/main/kotlin/year2022/day3/input").readLines()
+    val groups = sacks.chunked(3).map { group -> group.sortedBy { it.length } }
+    val prioritiesPerGroup = groups.map { group -> group.first().find { group[1].contains(it) && group[2].contains(it) }!!.priority() }
+    val prioritiesPerSack = sacks.map {
+        it.take(it.length / 2) to it.drop(it.length / 2)
+    }.map { sack ->
+        sack.first.find { sack.second.contains(it) }!!.priority()
+    }
+
+    println("part 1: ${prioritiesPerSack.sum()}")
+    println("part 2: ${prioritiesPerGroup.sum()}")
+}
